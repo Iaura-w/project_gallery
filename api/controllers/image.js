9477a0d413778ec4c043a47d8c116b95a5906c11
Iaura-w <laura.wo19@gmail.com>
2023-12-01 15:06:46 +0100

    add model.js

--- api/controllers/image.js
+++ api/controllers/image.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const { text } = require("express");
+var model = require("../model/model.js");
 
 module.exports = {
   listImages,
@@ -10,14 +11,15 @@ module.exports = {
   deleteImage,
 };
 
-var testData = {
+var testData;
+testData = new model.Image({
   id: "0123456789abcd",
   title: "Testowy obrazek",
   description: "Opis do obrazka",
   date: "2017-11-09T10:20:00.214Z",
   path: "/library/images/",
   size: 1024,
-};
+});
 
 function listImages(req, res, next) {
   res.json([testData]);
@@ -28,27 +30,25 @@ function createImage(req, res, next) {
 }
 
 function readImage(req, res, next) {
-  var id = req.swagger.params.id.value
+  var id = req.swagger.params.id.value;
 
   res.json(testData);
 }
 
 function updateImage(req, res, next) {
-  var id = req.swagger.params.id.value
-  var updated = testData
+  var id = req.swagger.params.id.value;
+  var updated = testData;
 
-  if (req.body['title'] != null) 
-    updated.title = req.body['title'];
-  if (req.body['description'] != null)
-    updated.description = req.body['description'];
-  if (req.body['date'] != null) 
-    updated.date = req.body['date'];
+  if (req.body["title"] != null) updated.title = req.body["title"];
+  if (req.body["description"] != null)
+    updated.description = req.body["description"];
+  if (req.body["date"] != null) updated.date = req.body["date"];
 
   res.json(updated);
 }
 
 function deleteImage(req, res, next) {
-  var id = req.swagger.params.id.value
+  var id = req.swagger.params.id.value;
   var del = {
     id: "fs4322",
     status: "deleted",
